c38a0ad15bf671aa79b7ce74ef0423cde0b44666
Bishant nayak <48652521+Ebulo@users.noreply.github.com>
2020-07-25 02:40:42 +0530

    Add files via upload

---
+++ precache-manifest.3eda3bacc6f4faa7cdac9cc0c8a00550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b8b0e58433d86e0214ebbde3a5e7d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1586628af2d019482696",
+    "url": "/static/css/main.c2d35f18.chunk.css"
+  },
+  {
+    "revision": "7b9404545db69382d09f",
+    "url": "/static/js/2.615ca00b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.615ca00b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1586628af2d019482696",
+    "url": "/static/js/main.ef82f337.chunk.js"
+  },
+  {
+    "revision": "5d1ba801342495187387",
+    "url": "/static/js/runtime-main.81d5bd28.js"
+  }
+]);
